a5750d1561e5eb42c1b50f1e52361bf216636fae
Ali Polatel <alip@exherbo.org>
2012-07-29 12:55:12 +0300

    test unlink

--- tests/t014-unlink.sh
+++ tests/t014-unlink.sh
@@ -1,9 +1,38 @@
 #!/bin/sh
 # vim: set sw=4 et ts=4 sts=4 tw=80 :
-# Copyright 2010 Ali Polatel <alip@exherbo.org>
+# Copyright 2010, 2012 Ali Polatel <alip@exherbo.org>
 # Distributed under the terms of the GNU General Public License v2
 
 test_description='sandbox unlink(2)'
 . ./test-lib.sh
+prog=t014_unlink
+
+test_expect_success setup '
+    touch file0 &&
+    touch file2
+'
+
+test_expect_success 'deny unlink()' '
+    test_must_violate sydbox \
+        -ESYDBOX_TEST_EPERM=1 \
+        -m core/sandbox/write:deny \
+        -- $prog file0 &&
+    test_path_is_file file0
+'
+
+test_expect_success 'deny unlink() for non-existant file' '
+    test_must_violate sydbox \
+        -ESYDBOX_TEST_EPERM=1 \
+        -m core/sandbox/write:deny \
+        $prog file1-non-existant
+'
+
+test_expect_success 'allow unlink()' '
+    sydbox -ESYDBOX_TEST_SUCCESS=1 \
+        -m core/sandbox/write:deny \
+        -m "whitelist/write+$HOME_RESOLVED/**" \
+        -- $prog file2 &&
+    test_path_is_missing file2
+'
 
 test_done
