alip/sydbox-1

commit 1c6cb723f6d731e06869fee2bacf1b497bba48ec

Simpler, more powerful logging

src/sydbox.c
@@ -68,6 +68,7 @@
 #include <getopt.h>
 
 #include "macro.h"
+#include "log.h"
 #include "util.h"
 #ifdef WANT_SECCOMP
 #include "seccomp.h"
@@ -115,7 +116,6 @@ usage: "PACKAGE" [-hVv] [-c pathspec...] [-m magic...] {-p pid...}\n\
    or: "PACKAGE" [-hVv] [-c pathspec...] [-m magic...] [-E var=val...] {command [arg...]}\n\
 -h          -- Show usage and exit\n\
 -V          -- Show version and exit\n\
--v          -- Be verbose, may be repeated\n\
 -c pathspec -- path spec to the configuration file, may be repeated\n\
 -m magic    -- run a magic command during init, may be repeated\n\
 -E var=val  -- put var=val in the environment for command, may be repeated\n\
@@ -243,7 +243,7 @@ static void sydbox_startup_child(char **argv)
 	if (strchr(filename, '/')) {
 		if (strlen(filename) > sizeof pathname - 1) {
 			errno = ENAMETOOLONG;
-			die_errno(1, "exec");
+			die_errno(2, "exec");
 		}
 		strcpy(pathname, filename);
 	}
@@ -331,7 +331,7 @@ static void sydbox_startup_child(char **argv)
 			PINK_EASY_PROCESS_IGNORE_ONE_SIGSTOP);
 	if (current == NULL) {
 		kill(pid, SIGKILL);
-		die_errno(1, "pink_easy_process_new");
+		die_errno(-1, "process_new failed, killed %lu", (unsigned long)pid);
 	}
 }
 
@@ -367,7 +367,7 @@ int main(int argc, char **argv)
 	   to children, but probably noone really needs that.  */
 	signal(SIGCHLD, SIG_DFL);
 
-	while ((opt = getopt_long(argc, argv, "hVvc:m:E:", long_options, &options_index)) != EOF) {
+	while ((opt = getopt_long(argc, argv, "hVc:m:E:", long_options, &options_index)) != EOF) {
 		switch (opt) {
 		case 0:
 			if (streq(long_options[options_index].name, "profile")) {
@@ -385,9 +385,6 @@ int main(int argc, char **argv)
 		case 'V':
 			about();
 			return 0;
-		case 'v':
-			sydbox->config.log_level++;
-			break;
 		case 'c':
 			config_reset();
 			config_parse_spec(optarg);
@@ -415,7 +412,6 @@ int main(int argc, char **argv)
 	}
 
 	pink_easy_init();
-	log_init();
 	config_done();
 	callback_init();
 	systable_init();
@@ -439,7 +435,7 @@ int main(int argc, char **argv)
 
 	sydbox->ctx = pink_easy_context_new(ptrace_options, &sydbox->callback_table, NULL, NULL);
 	if (sydbox->ctx == NULL)
-		die_errno(-1, "pink_easy_context_new");
+		die_errno(-1, "context_new failed");
 	pink_easy_context_set_step(sydbox->ctx, ptrace_default_step);
 
 	/* Ignore initial execve(2) related events

data/paludis
@@ -32,10 +32,10 @@
         }
     },
 
-    "log" : { "console_fd" : 2, /* Messages with level higher than message go to standard error as well. */
-              "file"       : "", /* Specify a path or leave it empty for no file logging. */
-              "level"      : 2, /* 0:fatal 1:warning 2:message 3:info 4:debug 5:trace */
-              "timestamp"  : true /* Prefix log messages with timestamp */
+    "log" : { "file"           : "", /* Specify a path or leave it empty for no file logging. */
+              "level"          : 0, /* log level, consult manual page */
+              "console_fd"     : 2, /* console file descriptor, default: stderr */
+              "console_level"  : 1 /* console log level, consult manual page */
             },
 
     "exec" : { "resume_if_match" : [ ],

man/sydbox.xml
@@ -134,11 +134,11 @@
           speficied using the <option>-c</option> command line switch or the <envar>SYDBOX_CONFIG</envar>
           environment variable. More than one configuration file may be specified this way. However, only the
           <emphasis role="bold">initial</emphasis> configuration file can change the <emphasis>core</emphasis>
-          configuration. If path to the configuration file is prefixed with the character
-          <emphasis role="bold">@</emphasis>, Sydbox looks for this configuration file under
-          <filename class="directory"><varname>$sharedir</varname>/sydbox/</filename>
-          where <varname>$sharedir</varname> is usually <filename class="directory">/usr/share</filename>.
-          The command line switch has precedence over the <envar>SYDBOX_CONFIG</envar> environment variable.
+          configuration. If path to the configuration file is prefixed with the character <literal>@</literal>,
+          Sydbox looks for this configuration file under
+          <filename class="directory"><varname>$sharedir</varname>/sydbox/</filename> where <varname>$sharedir</varname>
+          is usually <filename class="directory">/usr/share</filename>.  The command line switch has precedence over the
+          <envar>SYDBOX_CONFIG</envar> environment variable.
         </para>
       </listitem>
       <listitem>
@@ -590,26 +590,14 @@
         </varlistentry>
 
         <varlistentry>
-          <term><option id="log-console_fd">log/console_fd</option></term>
-          <listitem>
-            <para>type: <type>integer</type></para>
-            <para>
-              An integer specifying the console log file descriptor.
-              Defaults to 2, standard error.
-              See <xref linkend="logging"/> for more information.
-            </para>
-          </listitem>
-        </varlistentry>
-
-        <varlistentry>
           <term><option id="log-file">log/file</option></term>
           <listitem>
             <para>type: <type>string</type></para>
             <para>
               A string specifying the path to the log file.
               Defaults to "" (empty string), no logging to file.
-              See <xref linkend="logging"/> for more information.
             </para>
+            <para>See <xref linkend="logging"/> for more information.</para>
           </listitem>
         </varlistentry>
 
@@ -618,21 +606,34 @@
           <listitem>
             <para> type: <type>integer</type></para>
             <para>
-              An integer specifying the level of messages to log.
-              Log messages lower than this level are discarded.
-              Defaults to 2, <option>message</option>
+              An integer specifying the log level.
+              Defaults to 0, only fatal errors.
             </para>
+            <para>See <xref linkend="logging"/> for more information</para>
           </listitem>
         </varlistentry>
 
         <varlistentry>
-          <term><option id="log-timestamp">log/timestamp</option></term>
+          <term><option id="log-console_fd">log/console_fd</option></term>
           <listitem>
-            <para>type: <type>boolean</type></para>
+            <para>type: <type>integer</type></para>
             <para>
-              A boolean specifying whether the messages should be prefixed with a timestamp.
-              Defaults to <constant>true</constant>.
+              An integer specifying the console log file descriptor.
+              Defaults to 2, standard error.
             </para>
+            <para>See <xref linkend="logging"/> for more information</para>
+          </listitem>
+        </varlistentry>
+
+        <varlistentry>
+          <term><option id="log-console-level">log/console_level</option></term>
+          <listitem>
+            <para> type: <type>integer</type></para>
+            <para>
+              An integer specifying the log level.
+              Defaults to 1, only fatal errors and access violations.
+            </para>
+            <para>See <xref linkend="logging"/> for more information</para>
           </listitem>
         </varlistentry>
 
@@ -828,21 +829,79 @@
     <title>Logging</title>
 
     <para>
-      Sydbox has a simple logging infrastructure. There are two log targets, <option>core/log/console_fd</option> and
-      <option>core/log/file</option>. Messages with severity lower than <option>core/log/level</option> are discarded.
-      The rest are logged to <option>core/log/file</option> if specified and messages with log level higher than
-      <constant>message</constant> are logged to <option>core/log/console_fd</option> as well. Below is a list of log
-      levels and corresponding integers representing these levels:
+      Sydbox has a simple logging system. There are two log targets, <option>core/log/file</option> and
+      <option>core/log/console_fd</option>. Messages matching level <option>core/log/level</option> are logged to
+      <option>core/log/file</option>. Messages matching level <option>core/log/console_level</option> are logged to
+      <option>core/log/console_fd</option>. Below is a list of log levels and corresponding integers representing these
+      levels:
     </para>
 
-    <itemizedlist>
-      <listitem><para>0: fatal</para></listitem>
-      <listitem><para>1: warning</para></listitem>
-      <listitem><para>2: message</para></listitem>
-      <listitem><para>3: info</para></listitem>
-      <listitem><para>4: debug</para></listitem>
-      <listitem><para>5: trace</para></listitem>
-    </itemizedlist>
+    <table frame='all'>
+      <title>Sydbox Log Levels</title>
+      <tgroup cols='3' align='left' colsep='1' rowsep='1'>
+        <colspec colname='name' />
+        <colspec colname='value' />
+        <colspec colname='description' />
+        <thead>
+          <row>
+            <entry>Name</entry>
+            <entry>Value</entry>
+            <entry>Description</entry>
+          </row>
+        </thead>
+        <tbody>
+          <row>
+            <entry>ACCESS_V</entry>
+            <entry>1</entry>
+            <entry>Log access violations</entry>
+          </row>
+          <row>
+            <entry>INFO</entry>
+            <entry>2</entry>
+            <entry>Log messages about program workflow</entry>
+          </row>
+          <row>
+            <entry>ACCESS</entry>
+            <entry>4</entry>
+            <entry>Log denied/granted access</entry>
+          </row>
+          <row>
+            <entry>MAGIC</entry>
+            <entry>8</entry>
+            <entry>Log magic commands</entry>
+          </row>
+          <row>
+            <entry>CHECK</entry>
+            <entry>16</entry>
+            <entry>Log path/socket-address lookups and checks</entry>
+          </row>
+          <row>
+            <entry>MATCH</entry>
+            <entry>32</entry>
+            <entry>Log pattern matching, socket-address matching</entry>
+          </row>
+          <row>
+            <entry>TRACE</entry>
+            <entry>64</entry>
+            <entry>Log trace calls</entry>
+          </row>
+          <row>
+            <entry>SYSCALL</entry>
+            <entry>128</entry>
+            <entry>Log intercepted system calls</entry>
+          </row>
+          <row>
+            <entry>SYS_ALL</entry>
+            <entry>1024</entry>
+            <entry>Log all system calls</entry>
+          </row>
+        </tbody>
+      </tgroup>
+    </table>
+
+    <note>
+      <para>To select multiple debug levels, you can add them together.</para>
+    </note>
   </refsect1>
 
   <refsect1 id="sandboxing">
@@ -1150,10 +1209,10 @@
         }
     },
 
-    "log" : { "console_fd" : 2, /* Messages with level higher than message go to standard error as well. */
-              "file"       : "", /* Specify a path or leave it empty for no file logging. */
-              "level"      : 2, /* 0:fatal 1:warning 2:message 3:info 4:debug 5:trace */
-              "timestamp"  : true /* Prefix log messages with timestamp */
+    "log" : { "file"           : "", /* Specify a path or leave it empty for no file logging. */
+              "level"          : 0, /* log level, consult manual page */
+              "console_fd"     : 2, /* console file descriptor, default: stderr */
+              "console_level"  : 0 /* console log level, consult manual page */
             },
 
     "exec" : { "resume_if_match" : [ ],

src/Makefile.am
@@ -20,6 +20,7 @@ noinst_HEADERS= \
 		canonicalize.h \
 		file.h \
 		hashtable.h \
+		log.h \
 		macro.h \
 		path.h
 		proc.h \
@@ -34,6 +35,7 @@ sydbox_SOURCES= \
 		 canonicalize.c \
 		 file.c \
 		 hashtable.c \
+		 log.c \
 		 path.c \
 		 proc.c \
 		 seccomp.c \
@@ -53,7 +55,6 @@ sydbox_SOURCES= \
 		 sydbox-box.c \
 		 sydbox-callback.c \
 		 sydbox-config.c \
-		 sydbox-log.c \
 		 sydbox-magic.c \
 		 sydbox-panic.c \
 		 sydbox-path.c \
@@ -61,8 +62,8 @@ sydbox_SOURCES= \
 		 sydbox-sockinfo.c \
 		 sydbox-syscall.c \
 		 sydbox-systable.c \
-		 sydbox-util.c \
 		 sydbox-wild.c \
+		 sydbox-xfunc.c \
 		 sys-access.c \
 		 sys-chdir.c \
 		 sys-execve.c \

src/log.c
@@ -2,6 +2,8 @@
 
 /*
  * Copyright (c) 2010, 2011, 2012 Ali Polatel <alip@exherbo.org>
+ * Based in part upon privoxy which is:
+ *   Copyright (c) 2001-2010 the Privoxy team. http://www.privoxy.org/
  *
  * This file is part of Sydbox. sydbox is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -17,15 +19,19 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "sydbox-defs.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include <assert.h>
-#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <time.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
 
+#include "log.h"
 #include "util.h"
 
 #define ANSI_NORMAL		"[00;00m"
@@ -35,15 +41,30 @@
 #define ANSI_YELLOW		"[00;33m"
 #define ANSI_CYAN		"[00;36m"
 
+/* fatal can't be turned off! */
+#define LOG_LEVEL_MINIMUM	LOG_LEVEL_FATAL
+
+/* where to log (default: stderr) */
+static int logfd = -1;
+static int logcfd = STDERR_FILENO;
+
+/* logging detail level. */
+static int debug = (LOG_LEVEL_FATAL
+		| LOG_LEVEL_WARNING
+		| LOG_LEVEL_ACCESS_V
+		| LOG_LEVEL_INFO);
+static int cdebug = (LOG_LEVEL_FATAL
+		| LOG_LEVEL_WARNING
+		| LOG_LEVEL_ACCESS_V);
+
 static const char *prefix = LOG_DEFAULT_PREFIX;
 static const char *suffix = LOG_DEFAULT_SUFFIX;
-static int logfd = -1;
 
-PINK_GCC_ATTR((format (printf, 3, 0)))
-static inline void log_me(int fd, unsigned level, const char *fmt, va_list ap)
+PINK_GCC_ATTR((format (printf, 4, 0)))
+static void log_me(int fd, int level, const char *func, const char *fmt, va_list ap)
 {
 	int tty;
-	const char *p, *s;
+	const char *p, *s, *l;
 
 	tty = isatty(fd);
 
@@ -53,53 +74,66 @@ static inline void log_me(int fd, unsigned level, const char *fmt, va_list ap)
 		s = tty ? ANSI_NORMAL : "";
 		break;
 	case LOG_LEVEL_WARNING:
+	case LOG_LEVEL_ACCESS_V:
 		p = tty ? ANSI_MAGENTA : "";
-		s = tty ? ANSI_NORMAL : "";
-		break;
-	case LOG_LEVEL_NOTICE:
-		p = tty ? ANSI_GREEN : "";
 		s = tty ? ANSI_NORMAL : "";
 		break;
 	case LOG_LEVEL_INFO:
 		p = tty ? ANSI_YELLOW : "";
 		s = tty ? ANSI_NORMAL : "";
-		break;
-	case LOG_LEVEL_DEBUG:
-		p = tty ? ANSI_CYAN : "";
-		s = tty ? ANSI_NORMAL : "";
-		break;
 	default:
 		p = s = "";
 		break;
 	}
 
 	dprintf(fd, "%s", p);
-	if (prefix) {
-		if (sydbox->config.log_timestamp)
-			dprintf(fd, "%s@%lu: ", prefix, time(NULL));
-		else
-			dprintf(fd, "%s: ", prefix);
-	}
+	if (prefix)
+		dprintf(fd, "%s@%lu: ", prefix, time(NULL));
+	if (func)
+		dprintf(fd, "%s: ", func);
 	vdprintf(fd, fmt, ap);
 	dprintf(fd, "%s%s", s, suffix ? suffix : "");
 }
 
-void log_init(void)
+int log_init(const char *filename)
 {
-	assert(sydbox);
+	if (logfd > 0 && logfd != STDERR_FILENO)
+		close_nointr(logfd);
 
-	if (sydbox->config.log_file) {
-		logfd = open(sydbox->config.log_file, O_WRONLY|O_APPEND|O_CREAT, 0640);
+	if (filename) {
+		logfd = open(filename, O_WRONLY|O_APPEND|O_CREAT);
 		if (logfd < 0)
-			die_errno(3, "failed to open log file `%s'", sydbox->config.log_file);
+			return -errno;
+	} else {
+		logfd = -1;
 	}
+
+	log_debug_level(debug);
+	log_debug_console_level(cdebug);
+
+	return 0;
 }
 
 void log_close(void)
 {
-	if (logfd != -1)
+	if (logfd > 0)
 		close_nointr(logfd);
 	logfd = -1;
+}
+
+void log_console_fd(int fd)
+{
+	logcfd = fd;
+}
+
+void log_debug_level(int debug_level)
+{
+	debug = debug_level | LOG_LEVEL_MINIMUM;
+}
+
+void log_debug_console_level(int debug_level)
+{
+	cdebug = debug_level | LOG_LEVEL_MINIMUM;
 }
 
 void log_prefix(const char *p)
@@ -114,16 +148,10 @@ void log_suffix(const char *s)
 
 void log_msg_va(unsigned level, const char *fmt, va_list ap)
 {
-	if (level > sydbox->config.log_level)
-		return;
-
-	if (logfd != -1) {
-		log_me(logfd, level, fmt, ap);
-		if (level <= LOG_LEVEL_WARNING)
-			log_me(sydbox->config.log_console_fd, level, fmt, ap);
-	}
-	else
-		log_me(sydbox->config.log_console_fd, level, fmt, ap);
+	if (logfd > 0 && (level & debug))
+		log_me(logfd, level, NULL, fmt, ap);
+	if (logcfd > 0 && (level & cdebug))
+		log_me(logcfd, level, NULL, fmt, ap);
 }
 
 void log_msg(unsigned level, const char *fmt, ...)
@@ -132,5 +160,22 @@ void log_msg(unsigned level, const char *fmt, ...)
 
 	va_start(ap, fmt);
 	log_msg_va(level, fmt, ap);
+	va_end(ap);
+}
+
+void log_msg_va_f(unsigned level, const char *func, const char *fmt, va_list ap)
+{
+	if (logfd > 0 && (level & debug))
+		log_me(logfd, level, func, fmt, ap);
+	if (logcfd > 0 && (level & cdebug))
+		log_me(logcfd, level, func, fmt, ap);
+}
+
+void log_msg_f(unsigned level, const char *func, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	log_msg_va_f(level, func, fmt, ap);
 	va_end(ap);
 }

src/log.h
@@ -1,0 +1,71 @@
+/* vim: set cino= fo=croql sw=8 ts=8 sts=0 noet cin fdm=syntax : */
+
+/*
+ * Copyright (c) 2010, 2011, 2012 Ali Polatel <alip@exherbo.org>
+ * Based in part upon privoxy which is:
+ *   Copyright (c) 2001-2010 the Privoxy team. http://www.privoxy.org/
+ *
+ * This file is part of Sydbox. sydbox is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * sydbox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef LOG_H
+#define LOG_H 1
+
+#include <stdarg.h>
+#include <pinktrace/compiler.h>
+
+#define LOG_DEFAULT_PREFIX PACKAGE
+#define LOG_DEFAULT_SUFFIX "\n"
+
+/* Log levels */
+#define LOG_LEVEL_ACCESS_V	0x0001 /* log access violations */
+#define LOG_LEVEL_INFO		0x0002 /* log messages about program workflow */
+#define LOG_LEVEL_ACCESS	0x0004 /* log denied/granted access */
+#define LOG_LEVEL_MAGIC		0x0008 /* log magic commands */
+#define LOG_LEVEL_CHECK		0x0010 /* log path/socket-address lookups and checks */
+#define LOG_LEVEL_MATCH		0x0020 /* log pattern matching, socket-address matching */
+#define LOG_LEVEL_TRACE		0x0040 /* log trace calls */
+#define LOG_LEVEL_SYSCALL	0x0080 /* log intercepted system calls */
+#define LOG_LEVEL_SYS_ALL	0x0400 /* log all system calls */
+
+/* Log levels below are always on: */
+#define LOG_LEVEL_WARNING	0x0100
+#define LOG_LEVEL_FATAL		0x0200
+
+int log_init(const char *filename);
+void log_close(void);
+void log_console_fd(int fd);
+void log_debug_level(int debug_level);
+void log_debug_console_level(int debug_level);
+void log_prefix(const char *p);
+void log_suffix(const char *s);
+void log_msg_va(unsigned level, const char *fmt, va_list ap) PINK_GCC_ATTR((format (printf, 2, 0)));
+void log_msg_va_f(unsigned level, const char *func, const char *fmt, va_list ap) PINK_GCC_ATTR((format (printf, 2, 0)));
+void log_msg(unsigned level, const char *fmt, ...) PINK_GCC_ATTR((format (printf, 2, 3)));
+void log_msg_f(unsigned level, const char *func, const char *fmt, ...) PINK_GCC_ATTR((format (printf, 3, 4)));
+
+/* Shorthand notations */
+#define log_fatal(...)		log_msg_f(LOG_LEVEL_FATAL, __func__, __VA_ARGS__)
+#define log_warning(...)	log_msg_f(LOG_LEVEL_WARNING, __func__, __VA_ARGS__)
+#define log_access_v(...)	log_msg(LOG_LEVEL_ACCESS_V, __VA_ARGS__) /* treat specially */
+#define log_info(...)		log_msg_f(LOG_LEVEL_INFO, __func__, __VA_ARGS__)
+#define log_access(...)		log_msg_f(LOG_LEVEL_ACCESS, __func__, __VA_ARGS__)
+#define log_magic(...)		log_msg_f(LOG_LEVEL_MAGIC, __func__, __VA_ARGS__)
+#define log_match(...)		log_msg_f(LOG_LEVEL_MATCH, __func__, __VA_ARGS__)
+#define log_check(...)		log_msg_f(LOG_LEVEL_CHECK, __func__, __VA_ARGS__)
+#define log_trace(...)		log_msg_f(LOG_LEVEL_TRACE, __func__, __VA_ARGS__)
+#define log_syscall(...)	log_msg_f(LOG_LEVEL_SYSCALL, __func__, __VA_ARGS__)
+#define log_sys_all(...)	log_msg_f(LOG_LEVEL_SYS_ALL, __func__, __VA_ARGS__)
+
+#endif

src/magic-log.c
@@ -20,66 +20,49 @@
 #include "sydbox-defs.h"
 
 #include <stdlib.h>
-
+#include <errno.h>
 #include <pinktrace/pink.h>
 #include <pinktrace/easy/pink.h>
 
+#include "log.h"
 #include "macro.h"
 
-int magic_set_log_file(const void *val, PINK_GCC_ATTR((unused)) struct pink_easy_process *current)
+int magic_set_log_file(const void *val, struct pink_easy_process *current)
 {
-	const char *str = val;
+	int r;
+	const char *filename = val;
 
-	if (!str /* || !*str */)
+	if (!filename /* || !*filename */)
 		return MAGIC_ERROR_INVALID_VALUE;
 
 	log_close();
 
-	if (!*str) {
-		if (sydbox->config.log_file)
-			free(sydbox->config.log_file);
-		sydbox->config.log_file = NULL;
-		return 0;
+	if (*filename) {
+		if ((r = log_init(filename)) < 0) {
+			errno = -r;
+			die_errno(3, "log_init for file `%s' failed", filename);
+		}
+	} else {
+		log_init(NULL);
 	}
 
-	if (sydbox->config.log_file)
-		free(sydbox->config.log_file);
-	sydbox->config.log_file = xstrdup(str);
-
-	log_init();
-
 	return 0;
 }
 
-int magic_set_log_console_fd(const void *val, PINK_GCC_ATTR((unused)) struct pink_easy_process *current)
+int magic_set_log_level(const void *val, struct pink_easy_process *current)
 {
-	int r = PTR_TO_INT(val);
-
-	if (r < 0)
-		return MAGIC_ERROR_INVALID_VALUE;
-
-	sydbox->config.log_console_fd = r;
+	log_debug_level(PTR_TO_INT(val));
 	return 0;
 }
 
-int magic_set_log_level(const void *val, PINK_GCC_ATTR((unused)) struct pink_easy_process *current)
+int magic_set_log_console_fd(const void *val, struct pink_easy_process *current)
 {
-	int r = PTR_TO_INT(val);
-
-	if (r < 0 || r > 5)
-		return MAGIC_ERROR_INVALID_VALUE;
-
-	sydbox->config.log_level = r;
+	log_console_fd(PTR_TO_INT(val));
 	return 0;
 }
 
-int magic_set_log_timestamp(const void *val, PINK_GCC_ATTR((unused)) struct pink_easy_process *current)
+int magic_set_log_console_level(const void *val, struct pink_easy_process *current)
 {
-	sydbox->config.log_timestamp = PTR_TO_BOOL(val);
+	log_debug_console_level(PTR_TO_INT(val));
 	return 0;
-}
-
-int magic_query_log_timestamp(PINK_GCC_ATTR((unused)) struct pink_easy_process *current)
-{
-	return sydbox->config.log_timestamp;
 }

src/magic-socklist.c
@@ -26,6 +26,7 @@
 #include <pinktrace/easy/pink.h>
 
 #include "macro.h"
+#include "log.h"
 
 static int magic_set_socklist(const void *val, slist_t *head)
 {
@@ -50,14 +51,14 @@ static int magic_set_socklist(const void *val, slist_t *head)
 		case SYDBOX_MAGIC_ADD_CHAR:
 			errno = 0;
 			if ((r = sock_match_new(list[c], &match)) < 0) {
-				warning("invalid address `%s' (errno:%d %s)",
+				log_warning("invalid address `%s' (errno:%d %s)",
 						list[c], -r, strerror(-r));
 				r = MAGIC_ERROR_INVALID_VALUE;
 				goto end;
 			}
 			if (errno == EAFNOSUPPORT) {
 				/* ipv6 support disabled? */
-				info("unsupported address `%s' ignoring", list[c]);
+				log_magic("ignore unsupported address=`%s'", list[c]);
 				goto end;
 			}
 			node = xcalloc(1, sizeof(struct snode));

commit 0a35136b7e7efb6c3af9e0963771bf2befe1d206

fix --disable-seccomp broken by 1c6cb72

src/magic-trace.c
@@ -25,6 +25,7 @@
 #include <pinktrace/easy/pink.h>
 
 #include "macro.h"
+#include "log.h"
 
 int magic_set_trace_follow_fork(const void *val, struct pink_easy_process *current)
 {
@@ -53,7 +54,7 @@ int magic_set_trace_use_seccomp(const void *val, struct pink_easy_process *curre
 #ifdef WANT_SECCOMP
 	sydbox->config.use_seccomp = PTR_TO_BOOL(val);
 #else
-	info("seccomp support not enabled, ignoring magic");
+	log_magic("seccomp support not enabled, ignoring magic");
 #endif
 	return 0;
 }

src/sydbox.c
@@ -490,7 +490,7 @@ int main(int argc, char **argv)
 		ptrace_options |= PINK_TRACE_OPTION_SECCOMP;
 		ptrace_default_step = PINK_EASY_STEP_RESUME;
 #else
-		info("seccomp: not supported, disabling");
+		log_info("seccomp: not supported, disabling");
 		sydbox->config.use_seccomp = false;
 #endif
 	}