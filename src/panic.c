alip/sydbox-1

commit 58fa0b45493e95e745846cba664bdb16384f7f43

seccomp: do syscall checking in EVENT_SECCOMP
Before:
SECCOMP -> SYSENTRY -> BOX_CHECK -> DENY? -> (no) RESUME
					|
					|-> (yes) SYSEXIT -> RESTORE

After:
SECCOMP -> BOX_CHECK -> DENY? -> (no) RESUME
			    |
			    |-> (yes) SYSENTRY -> SYSEXIT -> RESTORE

Thus we stop the tracee one less time on successful syscall paths.
With this commit, the overhead of allowed system calls is reduced
noticably:

>>> Test: stat /dev/null 100000 times
Before:
	1: bare: 0.235686 sec
	2: sydbox [seize:0, seccomp:0]: 2.961659 sec
	3: sydbox [seize:0, seccomp:1]: 3.979262 sec (*)
	4: sydbox [seize:1, seccomp:0]: 2.930647 sec
	5: sydbox [seize:1, seccomp:1]: 4.012038 sec (*)
After:
	1: bare: 0.230615 sec
	2: sydbox [seize:0, seccomp:0]: 2.941883 sec
	3: sydbox [seize:0, seccomp:1]: 1.836790 sec (*)
	4: sydbox [seize:1, seccomp:0]: 2.937578 sec
	5: sydbox [seize:1, seccomp:1]: 1.836859 sec (*)

src/sydbox.c
@@ -735,8 +735,17 @@ static int event_exec(syd_proc_t *current)
 
 	if (sydbox->wait_execve) {
 		log_info("[wait_execve]: execve() ptrace trap");
+#ifndef WANT_SECCOMP
 		return 0;
+#else
+		if (sydbox->config.use_seccomp) {
+			log_info("[wait_execve]: sandboxing started");
+			sydbox->wait_execve = false;
+			return 0;
+		}
+#endif
 	}
+
 
 	if (current->flags & SYD_IGNORE_PROCESS)
 		return 0;
@@ -827,47 +836,69 @@ static int event_syscall(syd_proc_t *current)
 	int r = 0;
 
 	if (sydbox->wait_execve) {
+#ifndef WANT_SECCOMP
 		if (entering(current)) {
 			log_info("[wait_execve]: entering execve()");
+			current->flags |= SYD_INSYSCALL;
 		} else {
 			log_info("[wait_execve]: exiting execve(), sandboxing started");
+			current->flags &= ~SYD_INSYSCALL;
 			sydbox->wait_execve = false;
 		}
 		goto out;
+#else
+		/* nothing to do, seccomp trap handled this. */
+		;
+#endif
 	}
 
 	if (current->flags & SYD_IGNORE_PROCESS)
 		goto out;
 
-	r = entering(current) ? sysenter(current) : sysexit(current);
+	if (entering(current)) {
+#ifdef WANT_SECCOMP
+		if (!sydbox->config.use_seccomp || !sysdeny(current))
+			r = sysenter(current);
+#else
+		r = sysenter(current);
+#endif
+	} else {
+		r = sysexit(current);
+	}
 out:
-	current->flags ^= SYD_INSYSCALL;
-	if (sydbox->config.use_seccomp && sysexit(current))
+#ifdef WANT_SECCOMP
+	if (sydbox->config.use_seccomp && !entering(current))
 		current->trace_step = SYD_STEP_RESUME;
+#endif
+	current->flags ^= SYD_INSYSCALL;
 	return r;
 }
 
 #ifdef WANT_SECCOMP
 static int event_seccomp(syd_proc_t *current)
 {
+	int r;
+
 	if (sydbox->wait_execve) {
 		log_info("[wait_execve]: execve() seccomp trap");
-		goto out;
+		return 0;
 	}
+
+	if (current->flags & SYD_IGNORE_PROCESS)
+		return 0;
 
 #if 0
 	if ((r = syd_trace_geteventmsg(current, &ret_data)) < 0)
 		return ptrace_error(current, "PTRACE_GETEVENTMSG", -r);
-	if (current->flags & SYD_IGNORE_PROCESS)
-		return 0;
 #endif
 
-out:
-	/* Stop at syscall entry */
-	current->trace_step = SYD_STEP_SYSCALL;
-	current->flags &= ~SYD_INSYSCALL;
-
-	return 0;
+	r = sysenter(current);
+	if (sysdeny(current)) {
+		/* step using PTRACE_SYSCALL until we hit sysexit. */
+		current->flags &= ~SYD_INSYSCALL;
+		current->trace_step = SYD_STEP_SYSCALL;
+	}
+	return r;
 }
 #endif
 

commit 24f09942e8ab5b57b695ae689b23e10682496737

update sysexit handling broken by 58fa0b4

src/panic.c
@@ -105,7 +105,7 @@ static void report(syd_proc_t *current, const char *fmt, va_list ap)
 
 int deny(syd_proc_t *current, int err_no)
 {
-	current->flags |= SYD_DENYSYSCALL;
+	current->flags |= SYD_DENYSYSCALL | SYD_STOP_AT_SYSEXIT;
 	current->retval = errno2retval(err_no);
 
 	log_access("DENY retval:%ld errno:%d|%s|", current->retval,

src/sydbox.c
@@ -180,6 +180,7 @@ void clear_proc(syd_proc_t *p)
 	p->subcall = 0;
 	p->retval = 0;
 	p->flags &= ~SYD_DENYSYSCALL;
+	p->flags &= ~SYD_STOP_AT_SYSEXIT;
 
 	if (p->savebind)
 		free_sockinfo(p->savebind);
@@ -735,15 +736,13 @@ static int event_exec(syd_proc_t *current)
 
 	if (sydbox->wait_execve) {
 		log_info("[wait_execve]: execve() ptrace trap");
-#ifndef WANT_SECCOMP
-		return 0;
-#else
+#ifdef WANT_SECCOMP
 		if (sydbox->config.use_seccomp) {
 			log_info("[wait_execve]: sandboxing started");
 			sydbox->wait_execve = false;
-			return 0;
 		}
 #endif
+		return 0;
 	}
 
 
@@ -836,7 +835,10 @@ static int event_syscall(syd_proc_t *current)
 	int r = 0;
 
 	if (sydbox->wait_execve) {
-#ifndef WANT_SECCOMP
+#ifdef WANT_SECCOMP
+		if (sydbox->config.use_seccomp)
+			return 0;
+#endif
 		if (entering(current)) {
 			log_info("[wait_execve]: entering execve()");
 			current->flags |= SYD_INSYSCALL;
@@ -845,34 +847,39 @@ static int event_syscall(syd_proc_t *current)
 			current->flags &= ~SYD_INSYSCALL;
 			sydbox->wait_execve = false;
 		}
-		goto out;
-#else
-		/* nothing to do, seccomp trap handled this. */
-		;
-#endif
+		return 0;
 	}
 
 	if (current->flags & SYD_IGNORE_PROCESS)
-		goto out;
+		return 0;
 
 	if (entering(current)) {
 #ifdef WANT_SECCOMP
-		if (!sydbox->config.use_seccomp || !sysdeny(current))
-#endif
-		{
-			if ((r = UPDATE_REGSET(current)) < 0)
-				return ptrace_error(current, "PTRACE_GETREGSET", -r);
-			r = sysenter(current);
+		if (sydbox->config.use_seccomp &&
+		    (current->flags & SYD_STOP_AT_SYSEXIT)) {
+			log_trace("seccomp: skipping sysenter");
+			current->flags |= SYD_INSYSCALL;
+			return 0;
 		}
-	} else {
-		r = sysexit(current);
-	}
-out:
-#ifdef WANT_SECCOMP
-	if (sydbox->config.use_seccomp && !entering(current))
-		current->trace_step = SYD_STEP_RESUME;
 #endif
-	current->flags ^= SYD_INSYSCALL;
+		if ((r = UPDATE_REGSET(current)) < 0)
+			return ptrace_error(current, "PTRACE_GETREGSET", -r);
+		r = sysenter(current);
+#ifdef WANT_SECCOMP
+		if (sydbox->config.use_seccomp &&
+		    !(current->flags & SYD_STOP_AT_SYSEXIT)) {
+			log_trace("seccomp: skipping sysexit, resuming");
+			current->trace_step = SYD_STEP_RESUME;
+			return r;
+		}
+#endif
+		current->flags |= SYD_INSYSCALL;
+	} else {
+		if ((r = UPDATE_REGSET(current)) < 0)
+			return ptrace_error(current, "PTRACE_GETREGSET", -r);
+		r = sysexit(current);
+		current->flags &= ~SYD_INSYSCALL;
+	}
 	return r;
 }
 
@@ -897,7 +904,7 @@ static int event_seccomp(syd_proc_t *current)
 	if ((r = UPDATE_REGSET(current)) < 0)
 		return ptrace_error(current, "PTRACE_GETREGSET", -r);
 	r = sysenter(current);
-	if (sysdeny(current)) {
+	if (current->flags & SYD_STOP_AT_SYSEXIT) {
 		/* step using PTRACE_SYSCALL until we hit sysexit. */
 		current->flags &= ~SYD_INSYSCALL;
 		current->trace_step = SYD_STEP_SYSCALL;

src/sydbox.h
@@ -49,9 +49,10 @@
 #define SYD_FOLLOWFORK		00004
 #define SYD_INSYSCALL		00010
 #define SYD_DENYSYSCALL		00020
-#define SYD_IGNORE_PROCESS	00040
-#define SYD_SYDBOX_CHILD	00100
-#define SYD_WAIT_FOR_PARENT	00200
+#define SYD_STOP_AT_SYSEXIT	00040
+#define SYD_IGNORE_PROCESS	00100
+#define SYD_SYDBOX_CHILD	00200
+#define SYD_WAIT_FOR_PARENT	00400
 
 #define entering(p)	(!((p)->flags & SYD_INSYSCALL))
 #define exiting(p)	((p)->flags & SYD_INSYSCALL)

src/syscall-sock.c
@@ -75,6 +75,7 @@ int sys_bind(syd_proc_t *current)
 			current->savebind->addr = psa;
 			/* fall through */
 		default:
+			current->flags |= SYD_STOP_AT_SYSEXIT;
 			return 0;
 		}
 	}
@@ -202,8 +203,10 @@ int sys_getsockname(syd_proc_t *current)
 		return r;
 
 	ht_int64_node_t *node = hashtable_find(current->sockmap, fd + 1, 0);
-	if (node)
+	if (node) {
 		current->args[0] = fd;
+		current->flags |= SYD_STOP_AT_SYSEXIT;
+	}
 
 	return 0;
 }

src/syscall-special.c
@@ -204,6 +204,7 @@ int sys_dup(syd_proc_t *current)
 		return r;
 
 	current->args[0] = fd;
+	current->flags |= SYD_STOP_AT_SYSEXIT;
 	return 0;
 }
 
@@ -271,6 +272,7 @@ int sys_fcntl(syd_proc_t *current)
 		return r;
 
 	current->args[0] = fd;
+	current->flags |= SYD_STOP_AT_SYSEXIT;
 	return 0;
 }
 

src/syscall.c
@@ -364,6 +364,8 @@ int sysenter(syd_proc_t *current)
 		log_syscall("entering system call");
 		if (entry->enter)
 			return entry->enter(current);
+		else if (entry->exit)
+			current->flags |= SYD_STOP_AT_SYSEXIT;
 	} else {
 		log_sys_all("entering system call %ld", sysnum);
 	}