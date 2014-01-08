46166723850499b0cc6331bf72c3eef471fd452a
Ali Polatel <alip@exherbo.org>
2014-01-08 12:41:35 +0200

    fix prep-env.sh

--- misc/prep-env.sh
+++ misc/prep-env.sh
@@ -1,12 +1,7 @@
-#!/bin/sh
-
-ROOT="$(git rev-parse --show-toplevel 2>/dev/null)"
-if [[ -n "$ROOT" ]]; then
-    ROOT=$(readlink -f "${ROOT}")
-fi
+#!/bin/sh -x
 
 CFLAGS="-D__ALIP_WAS_HERE -O0 -g -ggdb3 -D__PINK_IS_BEHIND_THE_WALL"
-LD_LIBRARY_PATH="${ROOT}/pinktrace/.libs:${ROOT}/sydbox/.libs:${LD_LIBRARY_PATH}"
+PKG_CONFIG_PATH="$HOME/pink/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 export CFLAGS
-export LD_LIBRARY_PATH
+export PKG_CONFIG_PATH
